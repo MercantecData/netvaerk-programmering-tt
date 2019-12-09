41a249f21fe24948306e2e6eca20c4d61f93930f
Tommy <tmth.skp@edu.mercantec.dk>
2019-12-09 09:25:59 +0100

    Oversættelse af text til utf8

--- Projekt/Projekt/Program.cs
+++ Projekt/Projekt/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Projekt
 {
@@ -7,6 +8,18 @@ namespace Projekt
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Network!");
+            string text = "Morgenmad er ånsvagt godt!";
+            //Text oversat ved ascii
+            //var bytes = Encoding.ASCII.GetBytes(text);
+            
+            //Text oversat med utf8
+            var bytes = Encoding.UTF8.GetBytes(text);
+
+
+            //Send over netværk
+            text = Encoding.UTF8.GetString(bytes);
+            Console.WriteLine(text);
+
         }
     }
 }
