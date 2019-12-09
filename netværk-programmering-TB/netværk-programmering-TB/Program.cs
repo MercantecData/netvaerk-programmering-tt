ef7bd5ff62b9dd206b8bc4dbcb8a146571ab7240
Tobias Breinhild <tbbr.skp@edu.mercantec.dk>
2019-12-09 10:47:49 +0100

    Lavet opg. 1-4 i TB Branch

--- netværk-programmering-TB/netværk-programmering-TB/Program.cs
+++ netværk-programmering-TB/netværk-programmering-TB/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace netværk_programmering_TB
 {
@@ -7,6 +8,32 @@ namespace netværk_programmering_TB
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string text = Console.ReadLine();
+
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                //Opg. 1-2
+                var bytes = Encoding.ASCII.GetBytes(text);
+
+                text = Encoding.UTF8.GetString(bytes);
+                Console.WriteLine(text);
+            }
+            else if (input == "2")
+            {
+                //Opg. 3-4
+                var bytes = Encoding.UTF8.GetBytes(text);
+
+                text = Encoding.UTF8.GetString(bytes);
+                Console.WriteLine(text);
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input!");
+            }
+           
         }
     }
 }
