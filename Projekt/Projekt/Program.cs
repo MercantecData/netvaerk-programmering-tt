d494ad77d494326e17b9c0d4d4efa5b94166e309
Tommy <tmth.skp@edu.mercantec.dk>
2019-12-09 10:48:21 +0100

    Tlføjet readline og choice

--- Projekt/Projekt/Program.cs
+++ Projekt/Projekt/Program.cs
@@ -7,18 +7,35 @@ namespace Projekt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Network!");
-            string text = "Morgenmad er ånsvagt godt!";
-            //Text oversat ved ascii
-            //var bytes = Encoding.ASCII.GetBytes(text);
-            
-            //Text oversat med utf8
-            var bytes = Encoding.UTF8.GetBytes(text);
+            Console.WriteLine("Hello Network! \nWrite word: ");
+            string text = Console.ReadLine();
 
+            Console.WriteLine("No. 1 = ascii \nNo. 2 = Utf8");
+            int input=int.Parse(Console.ReadLine());
+            if (input == 1) {
 
-            //Send over netværk
-            text = Encoding.UTF8.GetString(bytes);
-            Console.WriteLine(text);
+                //Text oversat ved ascii opg 1-2
+                var bytes = Encoding.ASCII.GetBytes(text);
+
+                //Send over netværk
+                text = Encoding.UTF8.GetString(bytes);
+                Console.WriteLine(text);
+
+            }
+
+            else if (input == 2) {
+                //Text oversat med utf8 opg 3-4
+                var bytes = Encoding.UTF8.GetBytes(text);
+
+                //Send over netværk
+                text = Encoding.UTF8.GetString(bytes);
+                Console.WriteLine(text);
+            }
+
+            else { Console.WriteLine("No can do"); }
+
+            //opg 5
+            //UTF8Encoding[] bytes2 = {68, 97, 118};
 
         }
     }
