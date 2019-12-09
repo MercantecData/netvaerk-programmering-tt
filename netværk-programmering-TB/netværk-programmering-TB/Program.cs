5537d31f3515c855de00353986ef2f0151b78c15
Tobias Breinhild <tbbr.skp@edu.mercantec.dk>
2019-12-09 10:13:45 +0100

    Tilføjet nyt Visual Studio projekt i TB Branch

---
+++ netværk-programmering-TB/netværk-programmering-TB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace netværk_programmering_TB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
