ca7853c1c3663f9efeb42af019691bc49266236a
Tommy <tmth.skp@edu.mercantec.dk>
2019-12-11 11:11:38 +0100

    Async_upload

---
+++ Asynch_opg/ASyncClient/ASyncClientCode/ASyncClientCode/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace ASyncClientCode
+{
+    class Program
+    {
+        public static async void ReceiveMessage(NetworkStream stream)
+        {
+            byte[] buffer = new byte[256];
+
+            int numberOfBytesRead = await stream.ReadAsync(buffer, 0, 256);
+            string receivedMessage = Encoding.UTF8.GetString(buffer, 0, numberOfBytesRead);
+
+            Console.WriteLine("\n" + receivedMessage);
+        }
+
+        static void Main()
+        {
+            TcpClient client = new TcpClient();
+
+            int port = 13356;
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            //IPAddress ip = IPAddress.Parse("172.16.113.102");
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+
+            client.Connect(endPoint);
+
+            NetworkStream stream = client.GetStream();
+            ReceiveMessage(stream);
+
+            Console.WriteLine("Write your message here: ");
+            string text = Console.ReadLine();
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+
+            stream.Write(buffer, 0, buffer.Length);
+
+            client.Close();
+        }
+    }
+}
---
+++ Asynch_opg/opg_Async_2in1_server_og_client/opg_Async_2in1_server_og_client/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace opg_Async_2in1_server_og_client
+{
+    class Program
+    {
+        public async void ReceiveMessage(NetworkStream stream)
+        {
+            byte[] buffer = new byte[256];
+
+            int numberOfBytesRead = await stream.ReadAsync(buffer, 0, 256);
+            string receivedMessage = Encoding.UTF8.GetString(buffer, 0, numberOfBytesRead);
+
+            Console.Write("\n" + receivedMessage);
+        }
+
+        public static async void serverprog()
+        {
+            int port = 13356;
+            IPAddress ip = IPAddress.Any;
+            IPEndPoint localEndPoint = new IPEndPoint(ip, port);
+
+            TcpListener listener = new TcpListener(localEndPoint);
+
+            listener.Start();
+
+            Console.WriteLine("Awaiting Clients");
+            TcpClient client = await listener.AcceptTcpClientAsync(); //TcpClient client = listener.AcceptTcpClient();
+           
+
+            NetworkStream stream = client.GetStream();
+            ReceiveMessage(stream);
+
+            Console.Write("Write your message here: ");
+            string text = Console.ReadLine();
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+
+            stream.Write(buffer, 0, buffer.Length);
+            Console.ReadKey();
+
+        }
+       
+        public static void clientprog()
+        {
+            TcpClient client = new TcpClient();
+
+            int port = 13356;
+            IPAddress ip = IPAddress.Parse("127.0.0.1");//IPAddress ip = IPAddress.Parse("172.16.113.102");
+            
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+
+            client.Connect(endPoint);
+
+            NetworkStream stream = client.GetStream();
+            ReceiveMessage(stream);
+
+            Console.WriteLine("Write your message here: ");
+            string text = Console.ReadLine();
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+
+            stream.Write(buffer, 0, buffer.Length);
+            client.Close();
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nServer part: ");
+            serverprog();
+            
+            Console.WriteLine("\nClient part: ");
+            clientprog();
+        }
+    }
+}
